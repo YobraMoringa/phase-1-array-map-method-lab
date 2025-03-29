3bd4e1e78b3f1c45738e4d6d96484bcb5ba75221
Eric Brian Njuguna <brianerick.njuguna@gmail.com>
2025-03-29 19:52:18 +0300

    Added array methods and string methods in the function

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// STEPS
+// 1. Iterate array to get individual elements which are strings => sentenceElement
+// 2. Convert the string element into an array => sentenceIntoArrayElements
+// 3. Iterate over the newly created array(sentenceIntoArrayElements) using .map to gain access to individual elements which are the words
+// 4. Change 1st letter of every word in the element to uppercase and store the results in a new array(updatedSentenceIntoArrayElements)
+// 5. Convert the new array back to string and return it
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(sentenceElement => {
+    const sentenceIntoArrayElements = sentenceElement.split(" "); // Convert string to array
+    const updatedSentenceIntoArrayElements = sentenceIntoArrayElements.map(wordsAsIndividualElement => {
+      const updatedWord = wordsAsIndividualElement.replace(wordsAsIndividualElement.at(0), wordsAsIndividualElement.at(0).toUpperCase())// replace 1st character in the word with an uppercase one
+      return updatedWord;
+    })
+    return updatedSentenceIntoArrayElements.join(" "); //Convert array to string
+  })
 }
+
+console.log(titleCased());
